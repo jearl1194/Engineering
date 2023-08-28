a74b01d8864f92f2e5b9e759d314ac5133047ba8
jearl1194 <j.earl1194@gmail.com>
2023-08-28 17:34:22 +0800

    rafactor: methods to convert will reduce to two
    The methods to convert from a base to another base will reduce to two methods. These methods will be converting to decimal number and converting decimal number to another number system.

--- src/number-system.py
+++ src/number-system.py
@@ -2,10 +2,19 @@
 class Number:
     def __init__(self):
         self.type = Number
+
         self.decimal = None
         self.binary = None
         self.octal = None
         self.hexadecimal = None
+
+        self.decimal_symbols = {"0": 0, "1": 1, "2": 2, "3": 3,
+                                "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9}
+        self.binary_symbols = {"0": 0, "1": 1}
+        self.octal_symbols = {"0": 0, "1": 1, "2": 2,
+                              "3": 3, "4": 4, "5": 5, "6": 6, "7": 7}
+        self.hexadecimal_symbols = {"0": 0, "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6,
+                                    "7": 7, "8": 8, "9": 9, "A": 10, "B": 11, "C": 12, "D": 13, "E": 14, "F": 15}
 
     def __repr__(self):
         return f"Decimal:\t{self.decimal}\nBinary:\t\t{self.binary}\nOctal:\t\t{self.octal}\nHexadecimal:\t{self.hexadecimal}"
@@ -20,60 +29,69 @@ class Number:
         elif self.type == Hexadecimal:
             return self.hexadecimal
 
-
-class Decimal(Number):
-    def __init__(self, number):
-        Number.type = Decimal
-        Number.decimal = number
-        Number.binary = self._binary()
-        Number.octal = self._octal()
-        Number.hexadecimal = self._hexadecimal()
-
-    # Convert number to a different base
-    def convert(self, symbols):
-        """Convert a number to a different base by providing the symbols to use. The base will be computed from the number of symbols.
-
-        >>> convert(("0", "1", "2", "3"), 4)
-        """
+    # Convert a decimal number to a different base
+    def from_decimal(self, number, symbols):
         result = ""
+        number = int(number)
         base = len(symbols)
-        number = int(Number.decimal)
         while (number > 0):
             remainder = number % base
             number = number // base
-            result = symbols[remainder] + result
+            result = list(symbols.keys())[remainder] + result
         return result
 
-    # Convert number to binary
-    def _binary(self):
-        symbols = ("0", "1")
-        return self.convert(symbols)
+    # Convert a different base number to a decimal number
+    def to_decimal(self, number, symbols):
+        result = 0
+        number = list(number)
+        base = len(symbols)
+        for index in range(len(number)):
+            result += int(symbols[number.pop()]) * base ** index
+        return str(result)
 
-    # Convert number to octal
-    def _octal(self):
-        symbols = ("0", "1", "2", "3", "4", "5", "6", "7")
-        return self.convert(symbols)
 
-    # Convert number to hexadecimal
-    def _hexadecimal(self):
-        symbols = ("0", "1", "2", "3", "4", "5", "6", "7",
-                   "8", "9", "A", "B", "C", "D", "E", "F")
-        return self.convert(symbols)
+class Decimal(Number):
+    def __init__(Number, number):
+        super().__init__()
+        Number.type = Decimal
+        Number.decimal = number
+        Number.binary = Number.from_decimal(number, Number.binary_symbols)
+        Number.octal = Number.from_decimal(number, Number.octal_symbols)
+        Number.hexadecimal = Number.from_decimal(
+            number, Number.hexadecimal_symbols)
 
 
 class Binary(Number):
-    def __init__(self, number):
-        self.type = "Binary"
+    def __init__(Number, number):
+        super().__init__()
+        Number.type = Binary
         Number.binary = number
+        Number.decimal = Number.to_decimal(number, Number.binary_symbols)
+        Number.octal = Number.from_decimal(
+            Number.decimal, Number.octal_symbols)
+        Number.hexadecimal = Number.from_decimal(
+            Number.decimal, Number.hexadecimal_symbols)
 
 
 class Octal(Number):
-    def __init__(self, number):
-        self.type = "Octal"
+    def __init__(Number, number):
+        super().__init__()
+        Number.type = "Octal"
         Number.octal = number
+        Number.decimal = Number.to_decimal(number, Number.octal_symbols)
+        Number.binary = Number.from_decimal(
+            Number.decimal, Number.binary_symbols)
+        Number.hexadecimal = Number.from_decimal(
+            Number.decimal, Number.hexadecimal_symbols)
 
 
 class Hexadecimal(Number):
-    def __init__(self, number):
-        self.type = "Hexadecimal"
+    def __init__(Number, number):
+        super().__init__()
+        Number.type = "Hexadecimal"
         Number.hexadecimal = number
+        Number.decimal = Number.to_decimal(number, Number.hexadecimal_symbols)
+        Number.binary = Number.from_decimal(
+            Number.decimal, Number.binary_symbols)
+        Number.octal = Number.from_decimal(
+            Number.decimal, Number.octal_symbols)
