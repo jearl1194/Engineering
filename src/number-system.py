72ca5b9fbd9909412fd16554a2a827c66e77c171
jearl1194 <j.earl1194@gmail.com>
2023-08-31 01:02:17 +0800

    optimize code
    Optimize code by using built-in functions.

--- src/number-system.py
+++ src/number-system.py
@@ -8,14 +8,6 @@ class Number:
         self.octal = None
         self.hexadecimal = None
 
-        self.decimal_symbols = {"0": 0, "1": 1, "2": 2, "3": 3,
-                                "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9}
-        self.binary_symbols = {"0": 0, "1": 1}
-        self.octal_symbols = {"0": 0, "1": 1, "2": 2,
-                              "3": 3, "4": 4, "5": 5, "6": 6, "7": 7}
-        self.hexadecimal_symbols = {"0": 0, "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6,
-                                    "7": 7, "8": 8, "9": 9, "A": 10, "B": 11, "C": 12, "D": 13, "E": 14, "F": 15}
-
     def __repr__(self):
         return f"Decimal:\t{self.decimal}\nBinary:\t\t{self.binary}\nOctal:\t\t{self.octal}\nHexadecimal:\t{self.hexadecimal}"
 
@@ -29,69 +21,42 @@ class Number:
         elif self.type == Hexadecimal:
             return self.hexadecimal
 
-    # Convert a decimal number to a different base
-    def from_decimal(self, number, symbols):
-        result = ""
-        number = int(number)
-        base = len(symbols)
-        while (number > 0):
-            remainder = number % base
-            number = number // base
-            result = list(symbols.keys())[remainder] + result
-        return result
-
-    # Convert a different base number to a decimal number
-    def to_decimal(self, number, symbols):
-        result = 0
-        number = list(number)
-        base = len(symbols)
-        for index in range(len(number)):
-            result += int(symbols[number.pop()]) * base ** index
-        return str(result)
-
 
 class Decimal(Number):
     def __init__(Number, number):
         super().__init__()
         Number.type = Decimal
-        Number.decimal = number
-        Number.binary = Number.from_decimal(number, Number.binary_symbols)
-        Number.octal = Number.from_decimal(number, Number.octal_symbols)
-        Number.hexadecimal = Number.from_decimal(
-            number, Number.hexadecimal_symbols)
+        Number.decimal = int(number)
+        Number.binary = bin(number)
+        Number.octal = oct(number)
+        Number.hexadecimal = hex(number)
 
 
 class Binary(Number):
     def __init__(Number, number):
         super().__init__()
         Number.type = Binary
-        Number.binary = number
-        Number.decimal = Number.to_decimal(number, Number.binary_symbols)
-        Number.octal = Number.from_decimal(
-            Number.decimal, Number.octal_symbols)
-        Number.hexadecimal = Number.from_decimal(
-            Number.decimal, Number.hexadecimal_symbols)
+        Number.decimal = int(number, 2)
+        Number.binary = bin(Number.decimal)
+        Number.octal = oct(Number.decimal)
+        Number.hexadecimal = hex(Number.decimal)
 
 
 class Octal(Number):
     def __init__(Number, number):
         super().__init__()
-        Number.type = "Octal"
-        Number.octal = number
-        Number.decimal = Number.to_decimal(number, Number.octal_symbols)
-        Number.binary = Number.from_decimal(
-            Number.decimal, Number.binary_symbols)
-        Number.hexadecimal = Number.from_decimal(
-            Number.decimal, Number.hexadecimal_symbols)
+        Number.type = Octal
+        Number.decimal = int(number, 8)
+        Number.octal = oct(Number.decimal)
+        Number.binary = bin(Number.decimal)
+        Number.hexadecimal = hex(Number.decimal)
 
 
 class Hexadecimal(Number):
     def __init__(Number, number):
         super().__init__()
         Number.type = "Hexadecimal"
-        Number.hexadecimal = number
-        Number.decimal = Number.to_decimal(number, Number.hexadecimal_symbols)
-        Number.binary = Number.from_decimal(
-            Number.decimal, Number.binary_symbols)
-        Number.octal = Number.from_decimal(
-            Number.decimal, Number.octal_symbols)
+        Number.decimal = int(number, 16)
+        Number.hexadecimal = hex(Number.decimal)
+        Number.binary = bin(Number.decimal)
+        Number.octal = oct(Number.decimal)
